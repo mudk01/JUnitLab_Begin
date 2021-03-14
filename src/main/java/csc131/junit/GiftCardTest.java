908a812309a1db7d575318af70b07d351cc52029
Mueed Khalid <51312849+mudk01@users.noreply.github.com>
2021-03-13 16:57:41 -0800

    Added more tests and exception tests

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -1,9 +1,8 @@
 package csc131.junit;
 
-//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
 
 class GiftCardTest {
 
@@ -41,5 +40,38 @@ class GiftCardTest {
 		assertEquals("deduct(15.0)", expected, card.deduct(15.0));
 		
 	}
+	@Test
+	void deduct_AmountLessZero() {
+		double balance = 100.0;
+		int issuingStore = 1337;
+		GiftCard card = new GiftCard(issuingStore, balance);
+		String expected = "Invalid Transaction";
+		
+		assertEquals("deduct(-15.0)", expected, card.deduct(-15.0));
+		
+	}
+	@Test
+	void deduct_BalanceLessZero() {
+		double balance = 100.0;
+		int issuingStore = 1337;
+		GiftCard card = new GiftCard(issuingStore, balance);
+		String expected = "Amount Due: " + String.format("%6.2f", 750.0);
+		
+		assertEquals("deduct(850.0)", expected, card.deduct(850.0));
+		
+	}
+	@Test
+	void throwTest()throws IllegalArgumentException {
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(1,-100.0);});
+	}
+	@Test
+	void constructor_IncorrectID_Low() {
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(-1,100.0);});
+	}
+	@Test
+	void constructor_IncorrectID_High() {
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(10000, 100.0);});
+	}
+	
 
 }
